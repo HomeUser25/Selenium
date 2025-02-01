fa35caf8b4c40f61a95b92cdfeb44a479ca2d6c1
HomeUser25 <vladp2p@gmail.com>
2025-02-02 00:17:27 +0200

    Add files via upload

--- LoginPage.java
+++ LoginPage.java
@@ -8,34 +8,39 @@ import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage extends BasePage {
 
-    @FindBy(css="#user-name")
+    @FindBy(css = "#user-name")
     private WebElement userField;
 
-    @FindBy(css="#password")
+    @FindBy(css = "#password")
     private WebElement passwordField;
 
-    @FindBy(css="#user-name")
+    @FindBy(css = "#login-button")
     private WebElement loginBtn;
-
 
     public LoginPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
-
-    public void login (String user , String password ) throws InterruptedException {
-
-       driver.findElement(By.cssSelector("#user-name")).clear();
-       driver.findElement(By.cssSelector("#user-name")).sendKeys(user);
-       Thread.sleep(1000);
-       driver.findElement(By.cssSelector("#user-name")).clear();
-       driver.findElement(By.cssSelector("#password")).sendKeys(password);
-       Thread.sleep(1000);
-       driver.findElement(By.cssSelector("#login-button")).click();
-
-
-
-
+    public void setUserField(String user) {
+        userField.clear();
+        userField.sendKeys(user);
     }
 
+    public void setPasswordField(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginBtn.click();
+    }
+
+    public void login(String user, String password) throws InterruptedException {
+        setUserField(user);
+        Thread.sleep(1000);
+        setPasswordField(password);
+        Thread.sleep(1000);
+        clickLoginButton();
+    }
 }
